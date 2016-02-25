db1c6270c0a47deb5ad3cb4a3e0fc138e9037556
Ovidiu <vanghelie@pornhub.gov>
2016-02-25 10:50:20 +0200

    3
    3

---
+++ application/models/Model_users.php
@@ -0,0 +1,97 @@
+<?php
+
+// In modele bagi NUMAI ce apeluri faci la baza de date. SELECT UPDATE ETC. NU BAGI LOGICA (if for each chestii) in MODELE !!!
+
+class Model_users extends CI_Model
+{
+	public function is_user($username, $password)			//RETURN NR de randuri din DB cu nume si parola care se potrivesc cu ce ii dau eu call
+	{
+		
+	$query = $this->db->query("SELECT nume,parola from users WHERE nume='$username' and parola='$password'");
+	
+	return $query->num_rows();
+	
+	}
+		
+
+	public function is_admin($username)			// RETURN ADMIN =1 ROW
+	{
+		$query = $this->db->query("SELECT auth.admin FROM auth INNER JOIN users ON users.id_auth = auth.id_admin WHERE nume=('$username') and admin=('1')");
+		
+		return $query->row();
+	}
+
+	public function user_data($db_username)	// VIZUALIZARE RAND CU TOATE DATELE MODIFICABILE DIN DB PENTRU $db_username-ul PASAT
+	{
+		$query = $this->db->query("SELECT `users`.`nume`, `users`.`email`, `users`.`telefon`, `users`.`description`,`users`.`nume_buletin`, `age`.`g_varsta`, `auth`.`admin` FROM `age` LEFT JOIN `users` ON `users`.`id_age` = `age`.`id_varsta` LEFT JOIN `auth` ON `users`.`id_auth` = `auth`.`id_admin` WHERE nume=('$db_username')");
+		
+		return $query->row();
+	}
+	
+	
+		public function new_edit($nume, $new)
+	{
+		
+		$this->db->where('nume', $nume);
+		$this->db->update('users', $new);
+		
+
+
+	}
+	
+	
+
+	
+	public function age($nume, $age)
+	{
+		$query = $this->db->query("UPDATE `users` SET `id_age` = '$age' WHERE `users`.`nume` = '$nume';");
+	}
+	
+	
+	
+}
+// old code incoming 
+
+
+		
+		// NU mai baga logica in modele astea e doar pentru sa vb cu baza de date 
+	/*	if ($query->num_rows() == 1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+	public function is_user()				// nici post aici refacere sa fie doar fetch
+	{
+		$this->db->where('nume', $this->input->post('username'));
+		$this->db->where('parola', md5($this->input->post('password')));
+		
+		return $this->db->get('users');
+	}
+	
+	
+	
+	*/
+	 // SELECT * FROM users,auth WHERE users.id_auth=auth.id_auth 
+	
+	// CRED CA ASA VINE : SELECT 'users'.'nume', 'auth'.'admin' FROM 'auth' LEFT JOIN 'users' ON 'users'.'id_auth' = 'auth'.'id_admin'
+	// SELECT users.nume, auth.admin FROM auth INNER JOIN users ON users.id_auth = auth.id_admin 
+	
+	/*$query = $this->db->query('SELECT users.nume, auth.admin FROM auth INNER JOIN users ON users.id_auth = auth.id_admin'); // LIMIT 1 dupa auth.id_admin si ' 
+$row = $query->row();
+echo $row->admin;
+
+	ASTA SCOATE 1 CORECT IF ADMIN
+*/
+
+
+
+
+
+	
+	
---
+++ application/views/admins.php
@@ -0,0 +1,245 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+
+  <?php
+  // foloseste o librarie pentru data table si cu panel sa le fac frumix 
+  ?>
+  
+  
+<html lang="en">
+<head>
+  <title>ADMIN</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 20px;
+    }
+	
+	    header {
+      background-color: #a9a9a9;
+      padding: 20px;
+    }
+	
+	
+  </style>
+</head>
+<body>
+
+
+
+
+
+
+<nav class="navbar navbar-inverse">
+
+  <div class="container-fluid">
+
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">TEXT AICI DACA E NEVOIE</a>
+    </div>
+	
+	
+	<div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">SAU</a></li>
+        <li><a href="#">AICI</a></li>
+        <li><a href="#">ALTE</a></li>
+        <li><a href="#">BUTOANE</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  
+  </div>
+</nav>
+ 
+ 
+<header class="container-fluid text-center">    
+  <h1>ADMINISTRATOR</h1><br>
+</header>
+
+
+<div class "container text-center">
+<h1> </h1>
+<h1> </h1>
+</div>
+
+
+    </div>
+	   
+    </div>
+	   
+
+	   
+	   
+	   
+	   
+	   
+	   
+	   <div class="container">
+	   
+  
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>NUME</th>
+        <th>E-MAIL</th>
+        <th>TELEFON</th>
+		<th>DESCRIPTION</th>
+		<th>GRUPA VARSTA</th>
+		<th>PRIVILEGII ADMINISTRATIVE</th>
+		<th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>soon™</td>
+        <td>soon™</td>
+        <td>soon™</td>
+		
+        <td>soon™</td>
+        <td>soon™</td>
+        <td>soon™</td>
+		<td> <div class="pull-right"><button type="button" class="btn btn-warning">EDIT</button></div></td>
+		
+      </tr>
+      <tr>
+        <td>soon™</td>
+        <td>soon™</td>
+        <td>soon™</td>
+		
+        <td>soon™</td> 
+		<td>soon™</td>
+		<td>soon™</td>
+		<td> <div class="pull-right"><button type="button" class="btn btn-warning">EDIT</button></div></td>
+      </tr>
+      <tr>
+        <td>soon™</td>
+        <td>soon™</td>
+        <td>soon™</td>
+		
+        <td>soon™</td>
+        <td>soon™</td>
+        <td>soon™</td>
+		<td> <div class="pull-right"><button type="button" class="btn btn-warning">EDIT</button></div></td>
+		
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+
+
+
+<br>
+
+<footer class="container-fluid text-center">
+ <a href="http://www.cloud-ro.com/"> <h2>FOOTER PENTRU DACA O SA VREAU SA PUN CEVA AICI</h2></a>
+</footer>
+
+</body>
+</html>
+
+</body>
+</html>
